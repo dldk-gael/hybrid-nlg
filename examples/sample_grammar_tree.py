02ba0fd1e4a075722866cc5b89b3122c2c63364c
dldk <gael.dldk@pm.me>
2020-11-03 12:15:01 +0100

    add example with dead end branches

--- examples/sample_grammar_tree.py
+++ examples/sample_grammar_tree.py
@@ -11,20 +11,20 @@ def print_all_leaves(root):
         queue  = new_queue
 
 toy_cfg_grammar = """
-ROOT -> np vp
+s -> np vp
 np -> 'gael'
 vp -> v o 
 v -> 'knows'
 o -> 'bas' | 'judith'
 """
 
-grammar_root = GrammarNode.from_string(toy_cfg_grammar)
+grammar_root = GrammarNode.rootnode_from_grammar(toy_cfg_grammar, start_symbol="s")
 print(toy_cfg_grammar)
 print_all_leaves(grammar_root)
 print("-"*50)
 
 toy_feature_grammar = """
-ROOT -> NP[pos=subj] V NP[pos=obj] 
+S -> NP[pos=subj] V NP[pos=obj] 
 NP[pos=Pos] -> Pronoun[pos=Pos] | Proper[pos=Pos] 
 V -> "knows"
 Pronoun[pos=subj] -> "he"
@@ -34,6 +34,20 @@ Proper[pos=obj] -> "Bas"
 """
 
 print(toy_feature_grammar)
-grammar_root = GrammarNode.from_string(toy_feature_grammar)
+grammar_root = GrammarNode.rootnode_from_grammar(toy_feature_grammar, start_symbol="S")
+print_all_leaves(grammar_root)
+print("-"*50)
+
+
+feature_grammar_with_dead_end_branches = """
+S[sem=SEM] -> NP[sem=SEM,pos=subj] V NP[sem=SEM,pos=obj] 
+NP[sem=s1,pos=subj] -> "Gael"
+NP[sem=s1,pos=obj] -> "Bas"
+NP[sem=s2,pos=subj] -> "Bas"
+V -> "knows"
+"""
+
+print(feature_grammar_with_dead_end_branches)
+grammar_root = GrammarNode.rootnode_from_grammar(feature_grammar_with_dead_end_branches, start_symbol="S")
 print_all_leaves(grammar_root)
 print("-"*50)
