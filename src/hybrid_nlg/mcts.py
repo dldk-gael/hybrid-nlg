22364db670dee452d98849a3eff3be10df283edb
dldk <gael.dldk@pm.me>
2020-11-03 10:04:46 +0100

    improve comments

--- src/hybrid_nlg/mcts.py
+++ src/hybrid_nlg/mcts.py
@@ -1,12 +1,14 @@
 """
-This module implements MCTS for grammar search. It defines 4 classes : 
-- CounterNode -> maintain statistics on the visited part of the tree 
-- EvalBuffer -> use a buffer system in order to score the sentences by batch and not one by one 
+mcts module implement Monte Carlo Tree Search algorithm for grammar search. 
+It defines 4 classes : 
+- CounterNode -> node wrapper that maintains statistics on the visited part of the tree 
+- EvalBuffer -> buffer system use to score the sentences by batch and not one by one 
 - ResssourceDistributor -> handle the division of the computationnal ressource 
 - MCTS -> implement the classical MCTS steps : selection, expansion, simulation, backpropagation 
 """
 
 import math
+from typing import *
 
 from .lm_scorer import SentenceScore
 from .grammar_tree import GrammarNode
@@ -21,11 +23,11 @@ class CounterNode:
     - the square sum reward obtained so far from this node
     - a reference to the leaf corresponding to this top reward 
 
-    The counter node also keeps in memory a reference to his parent node in order to be able to backpropagate
+    The counter node also keeps in memory a reference to his parent node in order to backpropagate
     the information that it will recieve.
     """
 
-    def __init__(self, reference_node: Node, parent: "CounterNode" = None):
+    def __init__(self, reference_node: GrammarNode, parent: "CounterNode" = None):
         self.reference_node = reference_node
         self._children = None
         self.parent = parent
@@ -38,12 +40,12 @@ class CounterNode:
         # Useful to analyse and debug MCTS
         self.top_leaf_node = None
 
-        # If true will prevent any backpropagation to the node's parent
+        # If true -> prevent any backpropagation to the node's parent
         self.freeze = False
 
     def expand(self):
         assert not self.reference_node.is_terminal(), "Try to expand from a terminal node"
-        self._children = [CounterNode(children_node, parent=self) for children_node in self.reference_node.children()]
+        self._children = [CounterNode(child_node, parent=self) for child_node in self.reference_node.children()]
         self._is_terminal = False
 
     def children(self) -> List["CounterNode"]:  # type:ignore
@@ -53,7 +55,7 @@ class CounterNode:
     def is_terminal(self) -> bool:
         return self._is_terminal
 
-    def backpropagate(self, new_reward, leaf):
+    def backpropagate(self, new_reward: float, leaf: GrammarNode):
         """
         Given a new_reward update the average reward, sum of square rewards and top reward
         and then pass the information to the parent node
@@ -71,9 +73,9 @@ class CounterNode:
         if self.parent is not None:
             self.parent.backpropagate(new_reward, leaf)
 
-    def top_child(self) -> "CounterNode":
+    def top_child(self) -> "GrammarNode":
         """
-        Return the children that have the best top_reward value
+        Return the child that has the best top_reward value
         """
         assert not self.is_terminal(), "Try to access children of a terminal node :%s" % str(self.reference_node)
         return max(self.children(), key=lambda child: child.top_reward)
@@ -89,45 +91,46 @@ class RessourceDistributor:
 
     def __init__(self, strategy: str, tree_root: GrammarNode, ressources: int):
         strategy = strategy if strategy else "ALL_FROM_ROOT"
-        self.depth_max: int
         self.ressources_to_consume = ressources
-        self.ressources_already_consumed = 0 
-        self.ressources_already_consumed_at_current_depth = 0 
-
-        self.current_depth = 1 
 
         if strategy == "ALL_FROM_ROOT":
             self.ressources_to_consume_at_current_depth = self.ressources_to_consume
-        
-        if strategy == "UNIFORM": 
+        elif strategy == "UNIFORM":
             average_depth = tree_root.estimate_mean_depth(nb_samples=50)
-            # Factor 1.2 in order to consume all the computational ressources before arriving to the bottom of the tree 
-            self.ressources_to_consume_at_current_depth = self.ressources_to_consume / average_depth * 1.2 
+            # Factor 1.2 in order to consume all the computational ressources before getting to the bottom of the tree
+            self.ressources_to_consume_at_current_depth = self.ressources_to_consume / average_depth * 1.2
+
+        self.reset()
 
     def set_new_position(self, depth):
-        self.current_depth = current_depth 
+        self.current_depth = current_depth
         self.ressources_already_consumed_at_current_depth = 0
 
     def consume_one_unit(self):
-        self.ressources_already_consumed += 1 
-        self.ressources_already_consumed_at_current_depth += 1 
+        self.ressources_already_consumed += 1
+        self.ressources_already_consumed_at_current_depth += 1
 
-    def available_ressources(self):
+    def still_has_ressources(self):
         return self.ressources_already_consumed < self.ressources_to_consume
 
     def should_go_down_into_the_tree(self):
         return self.ressources_already_consumed_at_current_depth >= self.ressources_to_consume_at_current_depth
 
+    def reset(self):
+        self.current_depth = 1
+        self.ressources_already_consumed = 0
+        self.ressources_already_consumed_at_current_depth = 0
+
+
 class EvalBuffer:
     """
-    The evaluation buffer works as follows:
-    1. use add methods to add new leaf that need to be evaluated
-    2. use pop_results to retrieve the results if there are available
-    --> the leaf are sent to the evaluation function by batch of buffer_size
+    The evaluation buffer store the leaves in memory until it is full. 
+    Then, the leaves are sent to the LM-based scorer in one single batch. 
     """
-    # This is a vanilla implementation of the evaluation buffer, 
-    # it does not take advantage of parallelization or memoization 
-    # but those possible optimization can be simply implemented by overiding some methods. 
+    # This is a vanilla implementation of the evaluation buffer,
+    # it does not take advantage of parallelization or memoization
+    # but those possible optimization can be simply implemented by 
+    # overiding some of the following methods.
 
     def __init__(
         self, buffer_size: int, lm_scorer: SentenceScore,
@@ -135,10 +138,13 @@ class EvalBuffer:
         self.lm_scorer = lm_scorer
         self.buffer_size = buffer_size
         self.buffer: List[Tuple[GrammarNode, CounterNode]] = []
-        self.results: List[Tuple[float, GrammarNode, Node]] = []
+        self.results: List[Tuple[float, GrammarNode, CounterNode]] = []
 
-    def add(self, frontier_counter_node: CounterNode, leaf: Node):
-        if not leaf: # case of dead-end branches 
+        self.best_sentence = ""
+        self.best_score = -1
+
+    def add(self, frontier_counter_node: CounterNode, leaf: GrammarNode):
+        if not leaf:  # case of dead-end branches
             self.results.append((0, None, frontier_counter_node))
         else:
             self.buffer.append((leaf, frontier_counter_node))
@@ -146,33 +152,52 @@ class EvalBuffer:
         if len(self.buffer) == self.buffer_size:
             self.force_eval()
 
-    def force_eval(self): 
+    def force_eval(self):
+        if len(self.buffer) == 0:
+            return []
+
         scores = self.lm_scorer.compute_score([str(grammar_leaf) for (grammar_leaf, _) in self.buffer])
-        results += [(score, leaf, frontier_counter_node) for score, (leaf, frontier_counter_node) in zip(scores, self.buffer)]
+
+        for score, (leaf, frontier_counter_node) in zip(scores, self.buffer):
+            self.results.append((score, leaf, frontier_counter_node))
+            if score > self.best_score:
+                self.best_score = score
+                self.best_sentence = str(leaf)
+
         self.buffer = []
-        return results
+        return self.pop_results()
 
     def pop_results(self):
         output = self.results
         self.results = []
         return output
 
+
 class MCTS:
     def __init__(
-        self, lm_scorer: SentenceScore, buffer_size: int = 1, nb_random_restarts=1,
+        self,
+        lm_scorer: SentenceScore,
+        allocation_strategy="ALL_FROM_ROOT",
+        buffer_size: int = 1,
+        nb_random_restarts=1,
     ):
-        self.lm_scorer = lm_scorer
-        self.buffer_size = buffer_size
+        self.allocation_strategy = "ALL_FROM_ROOT"
         self.nb_random_restarts = nb_random_restarts
+        self.eval_buffer = EvalBuffer(buffer_size, lm_scorer)
 
     def search(self, root: GrammarNode, nb_of_tree_walks: int):
         nb_tree_walks_per_search = nb_of_tree_walks // self.nb_random_restarts
-        self.ressource_distributor.initialization(ressources=nb_tree_walks_per_search, tree=root)
-        for i in range(self.nb_random_restarts):
-            self.ressource_distributor.reset_remaining_ressources(nb_tree_walks_per_search)
-            self._single_search(root)
+        self.ressource_distributor = RessourceDistributor(
+            strategy=self.allocation_strategy, tree_root=root, ressources=nb_tree_walks_per_search
+        )
 
-    def single_search(self, root: Node):
+        for i in range(self.nb_random_restarts):
+            self.ressource_distributor.reset()
+            self.single_search(root)
+
+        return self.eval_buffer.best_sentence, self.eval_buffer.best_score
+
+    def single_search(self, root: GrammarNode):
         counter_root = CounterNode(reference_node=root, parent=None)
         current_depth = 1
 
@@ -188,41 +213,42 @@ class MCTS:
 
             # 3. simulation
             n = frontier_counter_node.reference_node
-            while n is not None and n.is_terminal():
-                # the first condition is needed because a node can have no child while being nonterminal
-                n = n.random_children()
+            while n.is_terminal():
+                n = n.random_child()
+                if not n : # can occur when a node has no child while being nonterminal -> dead-end branch
+                    break 
             random_leaf = n
 
             # 4. evaluation
-            # contrary to vanilla MCTS, we use a buffer system for evaluation 
+            # contrary to vanilla MCTS, we use a buffer system for evaluation
             self.eval_buffer.add(frontier_counter_node, random_leaf)
-            results = self.eval_buffer.pop_results()
+            results = self.eval_buffer.pop_results() 
+            # most of the time, results is an empty list because the buffer is only evaluated when full 
 
-            # 5. backpropagation 
+            # 5. backpropagation (every buffer_size nb of steps)
             for reward, leaf, frontier_counter_node in results:
                 frontier_counter_node.backpropagate(reward, leaf)
 
             # After each iteration, we query the ressource distributor to know if we should continue
-            # to perform the tree walks from current root or if we should go down to the best children
-            if self.ressource_distributor.go_to_children():
+            # to perform the tree walks from current root or if we should go down to the best child
+            if self.ressource_distributor.should_go_down_into_the_tree():
                 # Force the evaluation of the leaves that still remain in the buffer
-                self.eval_buffer.force_eval()
-                self.backpropagation_phase()
+                for reward, leaf, frontier_counter_node in self.eval_buffer.force_eval():
+                    frontier_counter_node.backpropagate(reward, leaf)
 
                 # Freeze current_root to avoid modifying the counter in futur backprops
                 counter_root.freeze = True
 
-                # Go to the best child (other strategy can be implemented here)
+                # Go to the best child (other strategies could be implemented here)
                 counter_root = max(counter_root.children(), key=lambda child: child.top_reward)
                 current_depth += 1
                 self.ressource_distributor.set_new_position(current_depth)
 
         self.eval_buffer.force_eval()
 
-
     ### SELECTION PHASE ####
-    # Go down to the frontier of the visited tree 
-    # by sucessively visiting the child that maximises the single player UCB 
+    # Go down to the frontier of the visited tree
+    # by sucessively visiting the child that maximises the single player UCB
 
     def selection_phase(self, root: CounterNode) -> CounterNode:
         node = root
@@ -233,11 +259,10 @@ class MCTS:
         return node
 
     def selection_policy(self, counter_node: CounterNode) -> CounterNode:
-        # if a children of current node has not been visited yet : visit it first 
-        for children in counter_node.children():
-            if children.count == 0:
-                return children
-
+        # if one child of current node has not been visited yet : visit it first
+        for child in counter_node.children():
+            if child.count == 0:
+                return child
         # else select the node that maximise the UCB
         return max(counter_node.children(), key=lambda node: self.single_player_ucb(node, counter_node))
 
